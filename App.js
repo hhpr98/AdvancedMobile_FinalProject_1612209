4a1b7ec96da1cb64378d85c77051e039d0c2e480
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-05-22 21:04:16 +0700

    generate basic component

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <>
+            <StatusBar barStyle="light-content"/>
+            <SafeAreaView>
+              <Text>aaaa</Text>
+            </SafeAreaView>
+        </>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
