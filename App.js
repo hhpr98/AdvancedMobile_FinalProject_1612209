857fbee1999c2d1853d7e4b25d230366e817ddd6
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-05-22 23:50:30 +0700

    finish download screen

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import Home from "./src/components/Main/Home/home";
+import Download from "./src/components/Main/Download/download";
 
 export default function App() {
     return (
         <>
             <StatusBar barStyle="light-content"/>
-            <SafeAreaView>
-                <Home></Home>
+            <SafeAreaView style={styles.main}>
+                <Download/>
             </SafeAreaView>
         </>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    main: {
+        flex:1,
+        justifyContent: 'center',
+    },
+});
 
---
+++ src/components/Main/Download/download.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const Download = () => {
+
+    return (
+        <View style={styles.home}>
+            <Image source={require('../../../../assets/icon8_download.png')} style={styles.image}/>
+            <Text style={styles.text1}>No downloads</Text>
+            <Text style={styles.text2}>Courses you download will appear here</Text>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    home: {
+        flex:1,
+        backgroundColor: 'black',
+        //flexDirection:'column',
+        //display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    image: {
+        width: 200,
+        height: 100
+    },
+    text1: {
+        margin: 20,
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 17,
+    },
+    text2: {
+        color: 'white',
+    }
+});
+
+export default Download;
