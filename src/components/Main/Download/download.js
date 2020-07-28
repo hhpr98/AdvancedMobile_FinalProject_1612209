06e0ea33a9828e85b99e42c69c5e5980d2e790e1
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-07-28 19:05:44 +0700

    feat: home screen

--- src/components/Main/Download/download.js
+++ src/components/Main/Download/download.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const Download = () => {
 
     return (
         <View style={styles.home}>
-            <Image source={require('../../../../assets/icon8_download.png')} style={styles.image}/>
+            <Image source={require('../../../../assets/icon8_download.png')} style={styles.image} />
             <Text style={styles.text1}>No downloads</Text>
             <Text style={styles.text2}>Courses you download will appear here</Text>
         </View>
@@ -15,12 +15,12 @@ const Download = () => {
 
 const styles = StyleSheet.create({
     home: {
-        flex:1,
+        flex: 1,
         backgroundColor: 'black',
         //flexDirection:'column',
         //display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     image: {
         width: 200,
--- src/components/Main/Home/Greeting/greeting.js
+++ src/components/Main/Home/Greeting/greeting.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 
 const Greeting = (props) => {
     const content = "This app is using for learing. For more information, please click Contact tab or call 772211. Let enjoys!";
@@ -16,19 +16,19 @@ const Greeting = (props) => {
 
 const styles = StyleSheet.create({
     image: {
-        height:220,
+        height: 220,
         resizeMode: "cover",
         justifyContent: "center"
     },
     title: {
-        margin:10,
-        color:'white',
+        margin: 10,
+        color: 'white',
     },
     content: {
-        margin:10,
-        color:'white',
-        fontSize:17,
-        fontWeight:'bold'
+        margin: 10,
+        color: 'white',
+        fontSize: 17,
+        fontWeight: 'bold'
     }
 });
 
