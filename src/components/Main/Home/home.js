8bc96dcfc6f6a890b6f70bf8d3b1a0b579f08e11
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-08-12 21:58:48 +0700

    feat: data provider for section cources

--- src/components/Main/Home/home.js
+++ src/components/Main/Home/home.js
@@ -3,6 +3,7 @@ import { ScrollView, StyleSheet } from 'react-native';
 import SectionCourses from "./SectionCourses/section-courses";
 import Greeting from "./Greeting/greeting";
 import { ThemeContext } from "./../../../../App";
+import { DataContext } from "../../../Provider/DataProvider";
 
 const Home = (props) => {
     return (
@@ -10,15 +11,22 @@ const Home = (props) => {
             {
                 ({ theme }) => {
                     return (
-                        <ScrollView style={{ ...styles.home, backgroundColor: theme.background }}>
-                            <Greeting name='Hòa' />
-                            <SectionCourses navigation={props.navigation} title='Software development' />
-                            <SectionCourses navigation={props.navigation} title='IT operations' />
-                            <SectionCourses navigation={props.navigation} title='Data professional' />
-                            <SectionCourses navigation={props.navigation} title='Security professional' />
-                        </ScrollView>
-
-                    );
+                        <DataContext.Consumer>
+                            {
+                                ({ data }) => {
+                                    return (
+                                        <ScrollView style={{ ...styles.home, backgroundColor: theme.background }}>
+                                            <Greeting name='Hòa' />
+                                            <SectionCourses navigation={props.navigation} title='Software development' courses={data.section.software} />
+                                            <SectionCourses navigation={props.navigation} title='IT operations' courses={data.section.it} />
+                                            <SectionCourses navigation={props.navigation} title='Data professional' courses={data.section.data} />
+                                            <SectionCourses navigation={props.navigation} title='Security professional' courses={data.section.secure} />
+                                        </ScrollView>
+                                    );
+                                }
+                            }
+                        </DataContext.Consumer>
+                    )
                 }
             }
         </ThemeContext.Consumer>
