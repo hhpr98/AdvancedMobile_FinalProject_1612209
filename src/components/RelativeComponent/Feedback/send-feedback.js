182adc2fb1893b4410f0f1da900c4b7af19333d3
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-08-22 15:41:27 +0700

    fix: issues #35

--- src/components/RelativeComponent/Feedback/send-feedback.js
+++ src/components/RelativeComponent/Feedback/send-feedback.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
 
-const ABC = (props) => {
+const FeedBack = (props) => {
+
+    const [text, setText] = useState('');
+
     return (
-        <View>
+        <View style={styles.home}>
+            <TextInput
+                style={styles.input}
+                onChangeText={text => setText(text)}
+                placeholder="Learning app feedback"
+                placeholderTextColor="white"
+                multiline={true}
+                numberOfLines={10} // only android
+            />
+            <TouchableOpacity style={styles.buttonSend} onPress={() => alert("Send feedback successfully!")}>
+                <Text style={styles.textSend}>Send</Text>
+            </TouchableOpacity>
+        </View >
 
-        </View>
-    );
+    )
 };
 
 const styles = StyleSheet.create({
     home: {
+        flexDirection: 'column',
         flex: 1,
-    }
+        backgroundColor: 'black'
+    },
+    input: {
+        height: 150,
+        margin: 20,
+        color: 'black',
+        borderColor: 'lightgray',
+        borderWidth: 1,
+        textAlign: "left",
+        backgroundColor: "white",
+    },
+    buttonSend: {
+        backgroundColor: 'black',
+        height: 40,
+        borderRadius: 10,
+        justifyContent: 'center',
+        borderColor: '#3399FF',
+        margin: 20,
+        borderWidth: 3,
+    },
+    textSend: {
+        color: '#3399FF',
+        fontSize: 17,
+        textAlign: 'center',
+    },
 });
 
-export default ABC;
+
+export default FeedBack;
